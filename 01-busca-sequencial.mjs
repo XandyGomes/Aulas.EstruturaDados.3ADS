c1856ca9be154eb1c08f19070a4b2370cbd935e1
XandyGomes <x3.computadores@hotmail.com>
2024-08-31 10:58:34 -0300

    Aula.31.08

--- 01-busca-sequencial.mjs
+++ 01-busca-sequencial.mjs
@@ -1,14 +1,16 @@
 import { nomes } from "./data/vetor-nomes.mjs";
-
+let comps
 function buscaSequencial(vetor, valorBusca) {
+  comps = 0
   for (let i = 0; i < vetor.length; i++) {
+    comps++
     if (vetor[i] === valorBusca) return i;
   }
   return -1;
 }
 
 console.time("Buscando ALEXANDRE...");
-console.log("Posição de ALEXANDRE: ", buscaSequencial(nomes, "ALEXANDRE"));
+console.log("Posição de ALEXANDRE: ", buscaSequencial(nomes, "ALEXANDRE"), "comparações: ", comps);
 console.timeEnd("Buscando ALEXANDRE...");
 
 // const frutas = ["laranja", "maçã", "banana", "melancia", "abacaxi"];

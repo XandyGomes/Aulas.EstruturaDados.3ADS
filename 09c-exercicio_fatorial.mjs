f3a71b9037db08c1459a40610787e4aa6dbc7385
XandyGomes <x3.computadores@hotmail.com>
2024-09-19 20:31:15 -0300

    Aula.19.09.2024

--- 09c-exercicio_fatorial.mjs
+++ 09c-exercicio_fatorial.mjs
@@ -1,6 +1,6 @@
 /*INSTRUÇÕES
 
-1. Faça uma função recursiva que receba um número inteiro positivo par N e
+1. Faça uma função recursiva que receba um número inteiro positivo N e
 imprima utilizando o console.log(), todos os números pares de 0 até N em
 ordem decrescente.
 

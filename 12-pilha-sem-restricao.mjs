cd9adca0f25d8a39001559d8463f681ae076daa2
XandyGomes <x3.computadores@hotmail.com>
2024-10-17 19:15:03 -0300

    Aula.10.10.2024

---
+++ 12-pilha-sem-restricao.mjs
@@ -0,0 +1,16 @@
+let vetor = []
+
+vetor.push(5)
+vetor.push(3)
+vetor.push(1)
+vetor.push(4)
+vetor.push(9)
+vetor.push(7)
+
+vetor.pop()
+vetor.pop()
+
+vetor.unshift(0)
+vetor.shift()
+
+console.log(vetor)
